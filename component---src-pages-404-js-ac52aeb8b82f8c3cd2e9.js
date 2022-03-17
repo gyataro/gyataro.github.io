47c19450540328f45bb24da9118c4a854acee162
gyataro <teoh6g@gmail.com>
2022-03-17 20:27:51 +0800

    Updates

---
+++ component---src-pages-404-js-ac52aeb8b82f8c3cd2e9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{6275:function(t,e,a){a.r(e);var n=a(9496),l=a(8696),s=a(1107);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ac52aeb8b82f8c3cd2e9.js.map
